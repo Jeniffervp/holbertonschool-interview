07f8bd962e94331eddd7bcad1d62105810ff98d4
jeniffervp <737@holbertonschool.com>
2020-04-18 21:12:31 -0500

    [Fix]cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,17 +2,15 @@
 
 
 def canUnlockAll(boxes):
-    flag = []
-    flag.insert(0, True)
-    for i in range(len(boxes) - 1):
-        prev = i-1
-        nxt = i+1
-        if nxt in boxes[i]:
-            flag.insert(nxt, True)
-        elif prev in boxes[i]:
-            flag.insert(prev, True)
-        else:
-            flag.insert(nxt, False)
+    flag = [None] * len(boxes)
+    flag[0] = True
+    for i in range(len(boxes)):
+        for j in range(len(boxes)):
+            if i in boxes[j] and i != j:
+                flag[i] = True
+                break
+        if flag[i] is not True:
+            flag[i] = False
     if False in flag:
         return False
     return True
