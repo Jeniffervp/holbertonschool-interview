b48e0674b5b5f08c6b36af60cf9f857e3e3b76b5
Jeniffervp <737@holbertonschool.com>
2020-05-02 22:30:33 +0000

    [Fix] conditions

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,9 +9,12 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new = 0;
 
 	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
 	if (new)
 	{
 		new->n = value;
