ca1aff949521980b947a430c72f454ce89486182
Jeniffervp <737@holbertonschool.com>
2020-05-11 20:01:43 +0000

    First commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+
+    if not n:
+        return 0
+
+    mov = 0
+
+    if n <= 2 or n % 2 == 1 and n % 3 == 1:
+        return n
+
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            mov += i
+            n = n / i
+        else:
+            i += 1
+
+    return mov
