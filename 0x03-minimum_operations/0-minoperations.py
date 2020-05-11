4ca52a82b754688ecddfc29258131631edc7e3fe
Jeniffervp <737@holbertonschool.com>
2020-05-11 20:26:56 +0000

    Fix pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -12,14 +12,11 @@ def minOperations(n):
         return 0
 
     mov = 0
-    if n % 2 == 1 and n % 3 == 1:
-        return n
-
     i = 2
     while i <= n:
         if n % i == 0:
             mov += i
-            n = n / i
+            n = n // i
         else:
             i += 1
 
