60956c764de837338560a43cacefbc7845b47079
Jeniffervp <737@holbertonschool.com>
2020-06-03 04:57:19 +0000

    [Fix] check the length

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,11 +12,12 @@ try:
     for fl in sys.stdin:
         metrics = fl.split(" ")
         status = metrics[-2]
-        size_file += int(metrics[-1])
-        cont += 1
 
-        if status in status_files.keys():
-            status_files[status] = status_files.get(status) + 1
+        if len(metrics) > 2:
+            size_file += int(metrics[-1])
+            cont += 1
+            if status in status_files.keys():
+                status_files[status] = status_files.get(status) + 1
         if cont % 10 == 0:
             print("File size: {}".format(size_file))
             for k, v in sorted(status_files.items()):
