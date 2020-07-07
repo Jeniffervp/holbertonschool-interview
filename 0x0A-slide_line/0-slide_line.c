569c5e34863a1ba55eb38982c1dc186828927208
Jeniffervp <737@holbertonschool.com>
2020-07-07 00:14:18 +0000

    [update] adding funtions to slide

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,53 +1,108 @@
 #include "slide_line.h"
 
 /**
- * slide_line - slides an array to the left or right to sum numbers
- * @line: line of elements
- * @size: Number of elements in an array
- * @direction: Left or Right
- * Return: 1 if possible, otherwise 0
+ * slide_line - slides an array to left or right to sum numbers
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * @direction: left or right
+ * Return: 1 if is possible or 0 if it's not possible
  */
 
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i, j;
-
+	if (direction == 2)
+		return (to_the_left(line, size));
 	if (direction == 1)
-	{
-		for (i = size; i > 0; i--)
-		{
-			for (j = size - 1; j > 0; j--)
-			{
-				if (line[i - 1] == 0)
-					continue;
-				if (i != j && line[i - 1] == line[j - 1])
-				{
-					line[i - 1] += line[j - 1];
-					line[j - 1] = 0;
-					break;
-				}
-			}
-		}
-		return (1);
-	}
-	else if (direction == 2)
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 1; j < size; j++)
-			{
-				if (line[i] == 0)
-					continue;
-				if (j != i && line[i] == line[j])
-				{
-					line[j] += line[i];
-					line[i] = 0;
-					break;
-				}
-			}
-		}
-		return (1);
-	}
+		return (to_the_right(line, size));
 	else
 		return (0);
+}
+
+/**
+ * to_the_left - slide to the left to sum
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
+ */
+
+int to_the_left(int *line, size_t size)
+{
+	int cur = 0, nxt = 0;
+	size_t i, index = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			nxt = line[i];
+		if (cur != 0 && nxt != 0)
+		{
+			if (cur == nxt)
+			{
+				line[index++] = cur + nxt;
+				cur = 0;
+				nxt = 0;
+			}
+			else
+			{
+				line[index++] = cur;
+				cur = nxt;
+				nxt = 0;
+				if (i == size - 1)
+					line[index++] = cur;
+			}
+		}
+		if (cur != nxt && i == size - 1)
+			line[index++] = cur;
+	}
+	for (i = index; i < size; i++)
+		line[i] = 0;
+	return (1);
+}
+
+/**
+ * to_the_right - slides an array to sum numbers
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
+ */
+
+int to_the_right(int *line, size_t size)
+{
+	int cur = 0, previous = 0;
+	size_t i, index = size - 1;
+
+	for (i = size - 1; i < size; i--)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			previous = line[i];
+		if (cur != 0 && previous != 0)
+		{
+			if (cur == previous)
+			{
+				line[index--] = cur + previous;
+				cur = 0;
+				previous = 0;
+			}
+			else
+			{
+				line[index--] = cur;
+				cur = previous;
+				previous = 0;
+				if (i == 0)
+					line[index--] = cur;
+			}
+		}
+		else if (cur != previous && i == 0)
+			line[index--] = cur;
+	}
+	for (i = 0; i < index + 1; i++)
+		line[i] = 0;
+	return (1);
 }
