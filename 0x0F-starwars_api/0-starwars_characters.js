f741dad712c418f57d916dd38740b7e75954d44c
Jeniffervp <737@holbertonschool.com>
2020-08-18 02:22:16 +0000

    [Fix]semistandart

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,33 +7,33 @@ const req = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 const charactersNames = () => {
-    // Request asynchronously the characters of a StarWars film and print it
+  // Request asynchronously the characters of a StarWars film and print it
 
-    req(url, async (error, response, body) => {
-	if (error) {
-	    console.log(error);
-	} else {
-	    const ch = JSON.parse(body).characters;
-	    for (const index in ch) {
-		const resp = await reqName(ch[index]);
-		console.log(resp);
-	    }
-	}
-    });
+  req(url, async (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const ch = JSON.parse(body).characters;
+      for (const index in ch) {
+        const resp = await reqName(ch[index]);
+        console.log(resp);
+      }
+    }
+  });
 };
 
 const reqName = (url) => {
-    // Return a promise with the character's names
+  // Return a promise with the character's names
 
-    return new Promise((resolve, reject) => {
-	req(url, (err, resp, body) => {
-	    if (err) {
-		reject(err);
-	    } else {
-		resolve(JSON.parse(body).name);
-	    }
-	});
+  return new Promise((resolve, reject) => {
+    req(url, (err, resp, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
     });
+  });
 };
 
 charactersNames();
